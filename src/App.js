7e6d3eb6185d1b9f1af87dce3ae5b656baf246e8
kero <kero13@livemail.tw>
2022-03-03 17:38:57 +0800

    add axios

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "./comp/Header";
-import { Tabs, Radio, Space } from "antd";
-// import { Button } from "antd";
-
+import { Radio } from "antd";
 import { allItems } from "./data/category";
 
 import "./App.css";
 
-const { TabPane } = Tabs;
-
 const App = () => {
-  // console.log(allItems);
   const [value4, setValue4] = useState("微糖豆漿");
+
   const onChange4 = (e) => {
-    console.log("radio4 checked", e.target.value);
     setValue4(e.target.value);
+    axios.get(`https://sheet1218.herokuapp.com/all`).then((res) => {
+      console.log(res.data);
+    });
   };
+
+  useEffect(() => {
+    // axios.get(`https://sheet1218.herokuapp.com/all`).then((res) => {
+    // axios.get(`http://localhost:9107/all`).then((res) => {
+    //   console.log(res.data);
+    // });
+  }, []);
+
   return (
     <div className="App">
       <Header></Header>
 
-      {/* <Radio.Group defaultValue="a" className="aa" size="large" type="primary">
-        {allItems.map((item) => (
-          <Radio.Button key={item.name} value={item.name}>
-            {item.name}
-          </Radio.Button>
-        ))}
-      </Radio.Group> */}
-
       <Radio.Group
         size="large"
-        className="aa"
+        className="RadioGroup"
         options={allItems}
         onChange={onChange4}
         value={value4}
