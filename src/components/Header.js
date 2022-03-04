8a26f7c937f9351798373c21f4b2755a00c50df2
kero <kero13@livemail.tw>
2022-03-04 16:36:55 +0800

    axios get

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import {
   AppstoreOutlined,
   SettingOutlined,
 } from "@ant-design/icons";
+import { axiosIns } from "../utilities/axios";
 
 export default function Header() {
   const [current, setCurrent] = useState("mail");
@@ -12,18 +13,24 @@ export default function Header() {
     setCurrent(e.key);
   };
 
+  const getAll = () => {
+    axiosIns.get(`all`).then((res) => {
+      console.log(res.data);
+    });
+  };
+
   return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
       <Menu.Item key="mail" icon={<MailOutlined />}>
         結帳
       </Menu.Item>
-      <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
+      <Menu.Item key="app" icon={<AppstoreOutlined onClick={getAll} />}>
         入庫
       </Menu.Item>
-      <Menu.Item key="app2" disabled icon={<AppstoreOutlined />}>
+      <Menu.Item key="app2" icon={<AppstoreOutlined />}>
         查詢
       </Menu.Item>
-      <Menu.Item key="app3" disabled icon={<SettingOutlined />}>
+      <Menu.Item key="app3" icon={<SettingOutlined />}>
         設定
       </Menu.Item>
     </Menu>
