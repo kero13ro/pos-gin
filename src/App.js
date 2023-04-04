97495332dcefb7a70511178ca03d33a8de9c0b0d
kero13ro <32509189+kero13ro@users.noreply.github.com>
2023-04-04 23:13:17 +0800

    fix: base url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "store";
 import Inventory from "components/Inventory";
@@ -15,20 +15,22 @@ import "style/utility.scss";
 import "style/vendor.scss";
 
 function App() {
+  // fix github baseurl
+  // https://www.freecodecamp.org/news/deploy-a-react-app-to-github-pages/
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="/inventory" element={<Inventory />} />
             <Route path="/restock" element={<Restock />} />
-            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/" element={<Checkout />} />
             <Route path="/search" element={<Search />} />
-            <Route path="/" element={<SignInScreen />} />
+            <Route path="/sign" element={<SignInScreen />} />
             <Route path="*" element={<p className="ma-16">網址輸入錯誤</p>} />
           </Route>
-        </Routes>
-      </BrowserRouter>
+        </Routes>      
+      </HashRouter>
     </Provider>
   );
 }
